11c6f967f604bf297d215a42fce4c4f2c60c48c2
wdlord <44736323+wdlord@users.noreply.github.com>
2022-05-10 13:32:50 -0700

    Updated to work with supplied template

--- ImageGen.py
+++ ImageGen.py
@@ -5,28 +5,26 @@ from PIL import ImageDraw
 from PIL import ImageFont
 
 MAX_WIDTH = 800                                 # max width that the name should take up
-TEMPLATE = "diploma_template.jpg"               # filename for the diploma template
-FONT = "times.ttf"                              # font filename (must be ttf, must be in same folder)
+TEMPLATE = "diploma_template.png"               # filename for the diploma template
+FONT = "Gilroy-Bold.ttf"                        # font filename (must be ttf, must be in same folder)
 
 NAME_FONT_SIZE = 65                             # default font size for the name on the diploma
-NAME_POSITION = (1536/2, 1154/2 - 100)          # center point for drawing the name (aligned center / base-point)
-
-DATE_FONT_SIZE = 40                             # default font size for the date on the diploma
-DATE_POSITION = (1200, 880)                     # center point for drawing the date (aligned center / base-point)
+NAME_POSITION = (1391 // 2, 983 // 2 - 80)      # center point for drawing the name (x_pos, y_pos)
+NAME_COLOR = (74, 79, 77)                       # color for the name on the diploma
 
 # test names and dates (temporary)
 test_data = [
-    ("Matthew Merrill", "04/16/22"),
-    ("Humongo-Long-Ass-Mother-Trucking-Giant-Name McDuff", "04/16/22")
+    "Matthew Merrill",
+    "Humongo-Long-Ass-Mother-Trucking-Giant-Name McDuff",
+    "James Hernandez-Chavez Smithsonian"
 ]
 
 
-def generate(name: str, date: str):
+def generate(name: str):
     """
     Generates a single diploma image given a name and date.
 
     :param name: The name that should appear on the diploma
-    :param date: The date that should appear on the diploma
     :return:
     """
 
@@ -44,17 +42,13 @@ def generate(name: str, date: str):
         name_font = ImageFont.truetype(FONT, font_size)
 
     # draw name text onto image
-    img_modified.text(NAME_POSITION, name, fill=(0, 0, 0), font=name_font, anchor="ms", align="center")
-
-    # create date font object and draw date text onto image
-    date_font = ImageFont.truetype(FONT, DATE_FONT_SIZE)
-    img_modified.text(DATE_POSITION, date, fill=(0, 0, 0), font=date_font, anchor="ms", align="center")
+    img_modified.text(NAME_POSITION, name, fill=NAME_COLOR, font=name_font, anchor="ms", align="center")
 
     # save the image
-    img.save(f"output/{name}_diploma.jpg")
+    img.save(f"output/{name}_diploma.png")
 
 
 # Runs test data if this is the main file
 if __name__ == "__main__":
     for item in test_data:
-        generate(*item)
+        generate(item)
