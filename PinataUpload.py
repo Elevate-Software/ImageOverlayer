af89758f23a99536e8b4baa9ee35e456257d34b1
wdlord <44736323+wdlord@users.noreply.github.com>
2022-05-11 15:45:41 -0700

    JSON metadata is now uploaded to IPFS as well.

--- PinataUpload.py
+++ PinataUpload.py
@@ -28,13 +28,33 @@ def pin_all_to_IPFS():
     filenames = ['output/' + fn for fn in os.listdir(os.getcwd() + '/output')]
 
     url = psa_endpoint + '/pinning/pinFileToIPFS'
+    json_url = psa_endpoint + '/pinning/pinJSONToIPFS'
     headers = {'Authorization': 'Bearer ' + jwt}
 
     for f in filenames:
+
+        # upload image to IPFS via Pinata API call
         files = {'file': open(f, "rb")}
         r = requests.post(url, headers=headers, files=files)
 
-        print(r.json())
+        # upload JSON metadata to IPFS via Pinata API call
+        json_metadata = construct_metadata(f, r.json()['IpfsHash'])
+        r = requests.post(json_url, headers=headers, json=json_metadata)
+
+
+def construct_metadata(filename, hash):
+    name = filename.split('_')[0].split('/')[1]
+    link = 'https://gateway.pinata.cloud/ipfs/' + hash
+    desc = f"This certifies that {name} has met the requirements for their exceptional performance in the " \
+           "Fundamentals of Decentralised Finance / Digital Asset Portfolio Management curriculum."
+
+    formatted_obj = {
+        'name': name,
+        'image_url': link,
+        'description': desc
+    }
+
+    return formatted_obj
 
 
 if __name__ == "__main__":
