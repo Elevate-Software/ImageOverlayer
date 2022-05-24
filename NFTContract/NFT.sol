5d9482ac81a8672175bda46c87d61d98c3a2da8a
Matthew Merrill <4mmerrill@gmail.com>
2022-05-23 20:49:30 -0700

    NFT.sol
    Uploaded the NFT contract

---
+++ NFTContract/NFT.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.0;
+ 
+import "https://github.com/0xcert/ethereum-erc721/src/contracts/tokens/nf-token-metadata.sol";
+import "https://github.com/0xcert/ethereum-erc721/src/contracts/ownership/ownable.sol";
+ 
+contract newNFT is NFTokenMetadata, Ownable {
+  uint256 public nextID;
+
+  constructor() {
+    nftName = "Synth NFT";
+    nftSymbol = "SYN";
+    nextID = 1;
+  }
+
+  function mint(address _to, uint256 _tokenId, string calldata _uri) external onlyOwner {
+    super._mint(_to, _tokenId);
+    super._setTokenUri(_tokenId, _uri);
+    nextID = nextID + 1;
+  }
+
+}
