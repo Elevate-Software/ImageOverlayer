963b94921f34795ae848f406f1fc4e34697b133c
wdlord <44736323+wdlord@users.noreply.github.com>
2022-06-03 16:42:28 -0700

    Diploma image now includes the token ID

--- ImageGen.py
+++ ImageGen.py
@@ -13,18 +13,24 @@ from PIL import ImageFont
 
 TEMPLATE = "diploma_template.png"               # filename for the diploma template
 FONT = "Gilroy-Bold.ttf"                        # font filename (must be ttf, must be in same folder)
+FONT_LIGHT = "Gilroy-Light.ttf"
 
 NAME_FONT_SIZE = 65                             # default font size for the name on the diploma
 NAME_POSITION = (1391 // 2, 983 // 2 - 80)      # center point for drawing the name (x_pos, y_pos)
 NAME_COLOR = (74, 79, 77)                       # color (r, g, b)
 MAX_WIDTH = 800                                 # max width that the name should take up
 
+TOKEN_FONT_SIZE = 20
+TOKEN_POSITION = (1320, 70)
+TOKEN_COLOR = (104, 109, 107)
 
-def generate(name: str):
+
+def generate(name: str, token: int):
     """
     Generates a single diploma image given a name and date.
 
     :param name: The name that should appear on the diploma.
+    :param token: NFT token ID for the image to be generated.
     :return:
     """
 
@@ -32,8 +38,9 @@ def generate(name: str):
     img = Image.open(TEMPLATE)
     img_modified = ImageDraw.Draw(img)
 
-    # create font object for name
+    # create font object for name and token watermark
     name_font = ImageFont.truetype(FONT, NAME_FONT_SIZE)
+    token_font = ImageFont.truetype(FONT_LIGHT, TOKEN_FONT_SIZE)
 
     # adjust name font size if output would be too long
     font_size = NAME_FONT_SIZE
@@ -44,6 +51,9 @@ def generate(name: str):
     # draw name text onto image
     img_modified.text(NAME_POSITION, name, fill=NAME_COLOR, font=name_font, anchor="ms", align="center")
 
+    # draw token ID onto image
+    img_modified.text(TOKEN_POSITION, f'Token-ID : {token}', fill=TOKEN_COLOR, font=token_font, anchor="rs", align="center")
+
     # save the image
     img.save(f"output/{name}_diploma.png")
 
@@ -51,10 +61,10 @@ def generate(name: str):
 # generates images for all names in the test data
 if __name__ == "__main__":
     test_data = [
-        "Matthew Merrill",
-        "Humongo-Long-Ass-Mother-Trucking-Giant-Name McDuff",
-        "James Hernandez-Chavez Smithsonian"
+        ("Matthew Merrill", 20),
+        ("Humongo-Long-Ass-Mother-Trucking-Giant-Name McDuff", 210020),
+        ("James Hernandez-Chavez Smithsonian", 22)
     ]
 
     for item in test_data:
-        generate(item)
+        generate(*item)
--- main.py
+++ main.py
@@ -8,6 +8,11 @@ import os
 # DEV: list 'users' and list 'pinned_uris' are both tuples sorted by user name (element 0)
 #       so that we can correctly associate user address to IPFS file.
 
+# initialize the mint factory
+contract_address = '0x96807aD777850A9336B9aD9F9Bb625CaD4eC0e5a'
+owner_address = '0x42A5243D51176bdCED13F40E3C85b7259e84c113'
+mint_factory = MintFactory(contract_address, owner_address, network='ropsten')
+
 # delete files in image folder before running to avoid any issues
 filenames = ['output/' + fn for fn in os.listdir(os.getcwd() + '/output')]
 for file in filenames:
@@ -18,9 +23,13 @@ with open("graduates.csv") as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     users = sorted([row for row in csv_reader])  # sorts by first value in sub-list
 
+# get starting token for generating contract images
+next_token = mint_factory.token
+
 # create diploma images for every name in list (discarding secondary value 'addr' with '_' char)
 for name, _ in users:
-    ImageGen.generate(name)
+    ImageGen.generate(name, next_token)
+    next_token += 1
 
 # upload all files to IPFS
 pinned_uris = PinataUpload.pin_all_to_IPFS()    # returns list of (name, uri) tuples
@@ -29,12 +38,7 @@ pinned_uris.sort()                              # sort by first value in tuple
 # create a new list of tuples of format: (address, uri)
 new_list = [(users[i][1], pinned_uris[i][1]) for i, _ in enumerate(users)]
 
-# create the MintFactory object
-contract_address = '0x96807aD777850A9336B9aD9F9Bb625CaD4eC0e5a'
-owner_address = '0x42A5243D51176bdCED13F40E3C85b7259e84c113'
-mint_factory = MintFactory(contract_address, owner_address, network='ropsten')
-
-# build transactions for all
+# build and call mint transactions for all
 for i, item in enumerate(new_list):
     return_txn = mint_factory.mint_nft(*item)
     print(users[i][0], return_txn.hex())     # name, txn hash
